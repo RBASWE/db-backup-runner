701841e5c751fa8dd19e8b0277e3110df55fdb85
RBASWE <r.bauernfeind@braun.at>
2024-08-12 13:26:33 +0200

    add delete cmd

---
+++ cmd/cron/delete.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2024 Robert Bauernfeind
+*/
+package cmd
+
+import (
+	"os/exec"
+	"path/filepath"
+
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/log"
+	"github.com/spf13/cobra"
+)
+
+var cronName string
+
+// deleteCmd represents the delete command
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete running cronjob - sudo required",
+	Long:  `Delete running cronjob - sudo required`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := delete(cronName); err != nil {
+			log.Error("Error on delete", "err", err)
+		}
+	},
+}
+
+func delete(cronName string) error {
+	delete := false
+	if err := huh.NewConfirm().Title("Proceed with delete?").Value(&delete).Run(); err != nil {
+		return err
+	}
+
+	if err := exec.Command("sudo", "rm", filepath.Join(CronDir, CronFilePrefix+"_"+cronName)).Run(); err != nil {
+		return err
+	}
+
+	log.Info("Deleted cronjob:" + cronName)
+
+	return nil
+}
+
+func init() {
+	deleteCmd.Flags().StringVarP(&cronName, "cronName", "c", "", "Name of the cronjob")
+	deleteCmd.MarkFlagRequired("cronName")
+	CronRootCmd.AddCommand(deleteCmd)
+}
