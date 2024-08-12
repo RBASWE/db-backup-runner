a6ceb3d6b5b9b100d01e4a405ad29e99e0173d51
RBASWE <r.bauernfeind@braun.at>
2024-08-12 10:02:13 +0200

    refactor form to huh

--- cmd/cron/add.go
+++ cmd/cron/add.go
@@ -6,11 +6,13 @@ package cmd
 import (
 	"errors"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
+	"regexp"
 	"runtime"
 
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/log"
 	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
@@ -29,14 +31,16 @@ type backupConfig struct {
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "Add new cron",
-	Long:  `add new cron`,
+	Short: "Add new cron - sudo required!",
+	Long:  `add new cron - sudo required!`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if name, cron, err := askForCron(); err != nil {
-			log.Fatal(err)
+			log.Error(err)
+			// log.Fatal(err)
 		} else {
 			if err := writeCron(name, cron); err != nil {
-				log.Fatal(err)
+				log.Error(err)
+				// log.Fatal(err)
 			}
 		}
 	},
@@ -62,9 +66,7 @@ func writeCron(cronName string, cron string) error {
 	defer file.Close()
 
 	// Move the file to the cron directory
-	// TODO check if file already exists
 	// TODO better solution?
-
 	if _, err := os.Stat(cronPath); err == nil {
 		prompt := promptui.Prompt{
 			Label:     "Do want to rename the file?",
@@ -106,192 +108,117 @@ func writeCron(cronName string, cron string) error {
 }
 
 func askForCron() (cronName string, cron string, err error) {
+
 	var (
+		pasteCron      string
 		cronExpression string
 		user           = "root"
 		backupCfg      = backupConfig{
-			dbType:     "",
+			dbType:     "pgsql",
 			host:       "",
-			port:       "",
+			port:       "5432",
 			user:       "",
 			password:   "",
 			dbName:     "",
 			output:     "",
 			maxFileAge: "",
 		}
-		fullCron     string
-		promptSelect promptui.Select
-		promptInput  promptui.Prompt
+		fullCron string
 	)
 
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().Title("Enter cron name").Value(&cronName).Validate(required),
+			huh.NewSelect[string]().Title("Do you want to paste a cron?").
+				Options(
+					huh.NewOption[string]("Yes", "yes"),
+					huh.NewOption[string]("No", "no").Selected(true),
+					huh.NewOption[string]("Read from file", "file"),
+				).Value(&pasteCron),
+		),
+	)
+
+	if err := form.Run(); err != nil {
+		return "", "", err
+	}
+
 	// TODO add valitadors to prompts
-
-	promptInput = promptui.Prompt{
-		Label:    "Cron name",
-		Validate: nil,
-	}
-	if cronName, err = promptInput.Run(); err != nil {
-		return "", "", err
-	}
-
-	promptSelect = promptui.Select{
-		Label: "Paste cron?",
-		Items: []string{"Yes", "No", "Read from file"},
-	}
-
-	i, _, err := promptSelect.Run()
-	if err != nil {
-		return "", "", err
-	}
-
-	switch i {
-	case 0:
-		promptInput = promptui.Prompt{
-			Label:    "Paste cron",
-			Validate: nil,
-		}
-		if fullCron, err = promptInput.Run(); err != nil {
+	switch pasteCron {
+	case "yes":
+		cronInput := huh.NewInput().
+			Title("Enter cron expression").
+			Description("Format: [cronExpression: * * * * *] [user: root] [task: pathToExecutable]").
+			Value(&fullCron).
+			Validate(required)
+		if err := cronInput.Run(); err != nil {
 			return "", "", err
 		}
 
-		return cronName, fullCron, nil
-	case 1:
-		var validator = func(input string) error {
-			if input == "" {
-				return errors.New("cannot be empty")
-			}
-			return nil
-		}
-		type input struct {
-			label        string
-			value        *string
-			inputType    string // prompt or select
-			options      []string
-			defaultValue string
-			validator    func(input string) error
+		log.Info(fullCron)
+		// return cronName, fullCron, nil
+	case "no":
+		cronBuilderForm := huh.NewForm(
+			huh.NewGroup(
+				huh.NewInput().Title("Enter cron expression").Value(&cronExpression).Validate(func(s string) error {
+					if err := required(s); err != nil {
+						return err
+					}
+					if err = validateCronExpression(s); err != nil {
+						return err
+					}
+
+					return nil
+				}),
+				// huh.NewInput().Title("Enter user").Value(&user).,
+				huh.NewSelect[string]().Title("Select database type").
+					Options(
+						// huh.NewOption[string]("MySQL", "mysql"),
+						huh.NewOption[string]("PostgreSQL", "pgsql").Selected(true),
+					).Value(&backupCfg.dbType),
+
+				huh.NewInput().Title("Database host").Value(&backupCfg.host).Validate(required),
+				huh.NewInput().Title("Database port").Value(&backupCfg.port).Validate(required),
+				huh.NewInput().Title("Database user").Value(&backupCfg.user).Validate(required),
+				huh.NewInput().Title("Database password").Value(&backupCfg.password).Validate(required).EchoMode(huh.EchoModePassword),
+				huh.NewInput().Title("Database name").Value(&backupCfg.dbName).Validate(required),
+				huh.NewInput().Title("Output directory").Value(&backupCfg.output).Validate(required),
+				huh.NewInput().Title("Max file age (default \"\" => no delete)").Value(&backupCfg.maxFileAge).Placeholder(""),
+			),
+		)
+
+		if err := cronBuilderForm.Run(); err != nil {
+			return "", "", err
 		}
 
-		inputs := []input{
-			{
-				label:        "Cron expression (* * * * *)",
-				value:        &cronExpression,
-				inputType:    "prompt",
-				defaultValue: "* * * * *",
-				validator:    validator,
-			},
-			{
-				label:     "Database type",
-				value:     &backupCfg.dbType,
-				inputType: "select",
-				options:   []string{"pgsql"}, // Add more options as needed
-				validator: validator,
-			},
-			{
-				label:        "Database host",
-				value:        &backupCfg.host,
-				inputType:    "prompt",
-				defaultValue: "192.168.35.43",
-				validator:    validator,
-			},
-			{
-				label:        "Database port",
-				value:        &backupCfg.port,
-				inputType:    "prompt",
-				defaultValue: "5432",
-				validator:    validator,
-			},
-			{
-				label:        "Database user",
-				value:        &backupCfg.user,
-				inputType:    "prompt",
-				defaultValue: "admin",
-				validator:    validator,
-			},
-			{
-				label:        "Database password",
-				value:        &backupCfg.password,
-				inputType:    "prompt",
-				defaultValue: "admin",
-				validator:    validator,
-			},
-			{
-				label:        "Database name",
-				value:        &backupCfg.dbName,
-				inputType:    "prompt",
-				defaultValue: "gseven",
-				validator:    validator,
-			},
-			{
-				label:        "Output directory",
-				value:        &backupCfg.output,
-				inputType:    "prompt",
-				defaultValue: "/home/rbaswe/backups",
-				validator:    validator,
-			},
-			{
-				label:        "Max file age (default \"\" => no delete)",
-				value:        &backupCfg.maxFileAge,
-				inputType:    "prompt",
-				defaultValue: "",
-				validator:    validator,
-			},
+		var command = ""
+		if binaryPath, err := os.Executable(); err != nil {
+			return "", "", err
+		} else {
+			command = binaryPath
 		}
 
-		for _, input := range inputs {
-			if input.inputType == "select" {
-				promptSelect = promptui.Select{
-					Label: input.label,
-					Items: input.options,
-				}
-				if _, *input.value, err = promptSelect.Run(); err != nil {
-					return "", "", err
-				}
-			} else {
+		command += " " + backupCfg.dbType + " --host " + backupCfg.host + " --port " + backupCfg.port + " --user " + backupCfg.user + " --pw " + backupCfg.password + " --database " + backupCfg.dbName + " --output " + backupCfg.output
 
-				promptInput = promptui.Prompt{
-					Label:    input.label,
-					Validate: input.validator,
-					Default:  input.defaultValue,
-				}
-				if *input.value, err = promptInput.Run(); err != nil {
-					return "", "", err
-				}
-			}
+		if backupCfg.maxFileAge != "" {
+			command += " --max-file-age " + backupCfg.maxFileAge
 		}
-	case 2:
+
+		fullCron = cronExpression + " " + user + " " + command
+
+	case "file":
 		return "", "", errors.New("option not supported yet")
 	}
 
-	var command = ""
-	if binaryPath, err := os.Executable(); err != nil {
+	fullCron += "\n"
+	log.Info(fullCron)
+
+	ok := false
+	confirmCron := huh.NewConfirm().Title("Confirm cron expression").Value(&ok)
+	if err := confirmCron.Run(); err != nil {
 		return "", "", err
 	} else {
-		command = binaryPath
-	}
-
-	command += " " + backupCfg.dbType + " --host " + backupCfg.host + " --port " + backupCfg.port + " --user " + backupCfg.user + " --pw " + backupCfg.password + " --database " + backupCfg.dbName + " --output " + backupCfg.output
-
-	if backupCfg.maxFileAge != "" {
-		command += " --max-file-age " + backupCfg.maxFileAge
-	}
-
-	fullCron = cronExpression + " " + user + " " + command + "\n"
-
-	fmt.Println("Cron expression: ", fullCron)
-
-	promptInput = promptui.Prompt{
-		Label:     "Please check the cron expresion before confirming",
-		Validate:  nil,
-		Default:   fullCron,
-		IsConfirm: true,
-	}
-
-	if confirm, err := promptInput.Run(); err != nil {
-		return "", "", err
-	} else {
-		if confirm == "y" {
+		if ok {
 			return cronName, fullCron, nil
-
 		} else {
 			return cronName, "", errors.New("cron expression not confirmed")
 		}
@@ -300,4 +227,24 @@ func askForCron() (cronName string, cron string, err error) {
 
 func init() {
 	CronRootCmd.AddCommand(addCmd)
+}
+
+// Form validators
+func required(s string) error {
+	if s == "" {
+		return errors.New("required")
+	}
+	return nil
+}
+
+func validateCronExpression(s string) error {
+	match, err := regexp.MatchString(`^(\*|[0-5]?\d)(/(\*|[1-5]?\d))?\s+(\*|[0-5]?\d)(-(\*|[0-5]?\d))?(/(\*|[1-5]?\d))?\s+(\*|[0-2]?\d|3[0-1])(-(\*|[0-2]?\d|3[0-1]))?(/(\*|[1-5]?\d))?\s+(\*|[1-9]|1[0-2])(-(\*|[1-9]|1[0-2]))?(/(\*|[1-5]?\d))?\s+(\*|[0-6]|7)(-(\*|[0-6]|7))?(/(\*|[1-5]?\d))?$`, s)
+	if err != nil {
+		return err
+	}
+	if !match {
+		return errors.New("invalid cron expression")
+	}
+
+	return nil
 }
